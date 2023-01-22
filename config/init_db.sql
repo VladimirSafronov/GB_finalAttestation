cd2ca2ec82cc0808deb9665826e055abbf260bac
VladimirSafronov <safron5000@mail.ru>
2023-01-22 18:35:08 +0600

    created connection to DB
    * added file with properties

---
+++ config/init_db.sql
@@ -0,0 +1,43 @@
+USE humanFriends;
+
+CREATE TABLE cat (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name CHAR(32),
+    age DATE,
+    commands TEXT
+);
+
+CREATE TABLE dog (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name CHAR(32),
+    age DATE,
+    commands TEXT
+);
+
+CREATE TABLE hamster (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name CHAR(32),
+    age DATE,
+    commands TEXT
+);
+
+CREATE TABLE horse (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name CHAR(32),
+    age DATE,
+    commands TEXT
+);
+
+CREATE TABLE camel (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name CHAR(32),
+    age DATE,
+    commands TEXT
+);
+
+CREATE TABLE donkey (
+	id INT PRIMARY KEY AUTO_INCREMENT,
+	name CHAR(32),
+    age DATE,
+    commands TEXT
+);
