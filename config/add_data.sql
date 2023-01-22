776e0101f533aaec33fd5185e73451c7f4a44394
VladimirSafronov <safron5000@mail.ru>
2023-01-22 21:30:42 +0600

    added data to DB

---
+++ config/add_data.sql
@@ -0,0 +1,42 @@
+INSERT INTO cat (name, age, commands)
+VALUES ('Leopold', '2023-01-01', ''),
+       ('Tiger', '2020-12-10', 'meow, ksks'),
+       ('Vaska', '2020-10-02', 'meow, eat'),
+       ('Myrka', '2021-03-13', 'meow'),
+       ('Leo', '2018-05-05', 'ksks, eat');
+
+INSERT INTO dog (name, age, commands)
+VALUES ('Barbos', '2023-01-10', ''),
+       ('Tyzik', '2020-10-10', 'voice, aport'),
+       ('Tyzemec', '2020-10-11', 'voice, eat'),
+       ('Dymka', '2021-03-13', 'voice, eat, atrush'),
+       ('Greta', '2018-10-05', 'voice, eat, fy');
+
+INSERT INTO hamster (name, age, commands)
+VALUES ('Ben', '2023-01-11', ''),
+       ('Denia', '2022-10-10', 'aport'),
+       ('Lena', '2021-10-11', 'eat'),
+       ('Galia', '2022-03-13', 'run, eat'),
+       ('Sema', '2019-10-05', 'eat, fly');
+
+INSERT INTO horse (name, age, commands)
+VALUES ('Prinz', '2023-01-15', ''),
+       ('Moon', '2020-05-10', 'aport'),
+       ('Tyzemec', '2018-10-11', 'voice, eat, run'),
+       ('Queen', '2017-03-13', 'eat, aport, run'),
+       ('Greta', '2016-10-05', 'voice, eat, jump');
+
+INSERT INTO camel (name, age, commands)
+VALUES ('Mystafa', '2023-01-01', ''),
+       ('Zyrab', '2015-10-10', 'eat'),
+       ('Achilles', '2019-10-11', 'voice, eat'),
+       ('Charles', '2001-03-13', 'voice, run'),
+       ('Merlin', '2008-10-05', 'voice, eat, run');
+
+INSERT INTO donkey (name, age, commands)
+VALUES ('Ace', '2023-01-11', ''),
+       ('Crown', '2018-03-10', 'run'),
+       ('Tyzemec', '2010-10-11', 'voice, eat'),
+       ('Ia', '2021-07-13', 'voice, eat, run'),
+       ('Shrek', '2019-11-07', 'voice, eat, fight');
+
